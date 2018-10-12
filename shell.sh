ce95769a04509ad900d87bb5c8fb60fae8e33448
lingjun.jlj <jlj_vip98@qq.com>
2018-10-12 09:30:44 +0800

    0

--- shell.sh
+++ shell.sh
@@ -3,7 +3,7 @@ echo '执行脚本生成目录结构'
 node createcatalogue.js
 echo '目录结构生成完毕！'
 echo '上传到GitHub上'
-commit_str
+commit_str = "fix JavaGuide"
 if [[ $# -lt 1  ]]; then
 	echo "have not any args,default a commit"
     commit_str = "fix JavaGuide"
