6960cdc5d6249f7dfa9afd53c8642fdd28f834bc
lingjun.jlj <jlj_vip98@qq.com>
2018-10-12 09:36:32 +0800

    0

--- shell.sh
+++ shell.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 echo "参数 $1"
-echo '执行脚本生成目录结构'
+echo "执行脚本生成目录结构"
 node createcatalogue.js
-echo '目录结构生成完毕！'
-echo '上传到GitHub上'
+echo "目录结构生成完毕！"
+echo "上传到GitHub上"
 commit_str = "fix JavaGuide"
 if [ $# -lt 1  ]; then
 	echo "have not any args,default a commit"
@@ -14,5 +14,5 @@ echo ${#commit_str}
 git add .
 git commit -m ${#commit_str}
 #git push origin master
-echo '上传GitHub完毕'
+echo "上传GitHub完毕"
 
