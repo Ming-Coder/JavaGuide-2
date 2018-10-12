05f4cfc16f778727917068d6c6ab6ac65f9de14d
lingjun.jlj <jlj_vip98@qq.com>
2018-10-12 09:43:52 +0800

    Java基础知识

--- shell.sh
+++ shell.sh
@@ -4,15 +4,9 @@ echo "执行脚本生成目录结构"
 node createcatalogue.js
 echo "目录结构生成完毕！"
 echo "上传到GitHub上"
-commit_str = "fix JavaGuide"
-if [ $# -lt 1  ]; then
-	echo "have not any args,default a commit"
-    commit_str = "fix JavaGuide"
-fi
-commit_str = $1
-echo ${#commit_str}
+echo $commit_str
 git add .
-git commit -m ${#commit_str}
-#git push origin master
+git commit -m "$1"
+git push origin master
 echo "上传GitHub完毕"
 
