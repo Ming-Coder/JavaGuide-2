7ce02faa0b886aae8be6afdec86fd58669f0a480
lingjun.jlj <jlj_vip98@qq.com>
2018-10-12 09:45:06 +0800

    Java基础知识

--- shell.sh
+++ shell.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-echo "参数 $1"
 echo "执行脚本生成目录结构"
 node createcatalogue.js
 echo "目录结构生成完毕！"
 echo "上传到GitHub上"
-echo $commit_str
+echo "commit参数 $1"
 git add .
 git commit -m "$1"
 git push origin master
