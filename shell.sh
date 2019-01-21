8592e95f3663eca0f92d72ed8e9f12a5bdce61cc
arthas <jlj_vip98@qq.com>
2019-01-21 11:51:39 +0800

    fix

--- shell.sh
+++ shell.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
+if [ x"$1" = x ]; then 
+    echo "填写 commit参数..."
+    exit 1
+fi
 echo "执行脚本生成目录结构"
 node createcatalogue.js
 echo "目录结构生成完毕！"
 echo "上传到GitHub上"
-echo "commit参数 $1"
 git add .
 git commit -m "$1"
 git push origin master
